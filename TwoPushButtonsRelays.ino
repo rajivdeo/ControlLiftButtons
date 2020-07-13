2dff44b8dc92759d4981b3002c5f76a6fc9a7e93
Rajiv Deo <rajivdeo@gmail.com>
2020-07-13 17:00:23 +0530

    Update TwoPushButtonsRelays.ino

--- TwoPushButtonsRelays.ino
+++ TwoPushButtonsRelays.ino
@@ -1,3 +1,6 @@
+/* ControlLiftButtons Arduino Code written by Rajiv B Deo July 2020
+   This code is in public domain and can be freely copied.
+   */
 int pbutton1Pin = 2;// connect output to push button
 int relay1Pin = 10;// Connected to relay (LED)
 int pbutton2Pin = 3;// connect output to push button
@@ -6,7 +9,6 @@ int relay2Pin = 11;// Connected to relay (LED)
 int val = LOW; // push value from input pin 
 
 void setup() {
-  // Robojax.com code and video tutorial for push button ON and OFF
   Serial.begin(115200);
   pinMode(pbutton1Pin, INPUT_PULLUP); 
   pinMode(relay1Pin, OUTPUT);
